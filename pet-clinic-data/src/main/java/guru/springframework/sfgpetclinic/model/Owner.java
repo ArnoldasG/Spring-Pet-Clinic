b9f88f4d63eda5553375ddc21a4cc869cd95461d
ArnoldasG <grevasarnoldas@gmail.com>
2021-05-07 12:47:23 +0300

    updated owner with JPA entity mappings

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,13 +1,20 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 
+    @Column(name = "address")
     private String address;
+    @Column(name = "city")
     private String city;
+    @Column(name = "telephone")
     private String telephone;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner") // If I delete owner - pets also will delete
     private Set<Pet> pets = new HashSet<>();
 
     // Getters and Setters
