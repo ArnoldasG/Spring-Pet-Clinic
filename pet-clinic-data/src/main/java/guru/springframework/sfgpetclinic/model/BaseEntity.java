695cfca78a86219b7c1b2520aa74576bf6927608
ArnoldasG <grevasarnoldas@gmail.com>
2021-05-07 12:34:57 +0300

    Updated Base Entity with JPA annotations

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,9 +1,17 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass // hey JPA we are going to inherit from this class so other classes are going to be inheriting it.
+// We don't need this specific class mapped to the database.
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     // GETTERS AND SETTERS
