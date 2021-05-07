b2b7022ee5594bf7d24049223a3950189f60f522
ArnoldasG <grevasarnoldas@gmail.com>
2021-05-07 18:06:17 +0300

    JPA mapping for Pet and Visit classes

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -2,6 +2,8 @@ package guru.springframework.sfgpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -18,6 +20,9 @@ public class Pet extends BaseEntity {
     private Owner owner;
     @Column(name = "birth_date")
     private LocalDate birtDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
 
     public String getName() {
@@ -50,5 +55,13 @@ public class Pet extends BaseEntity {
 
     public void setBirtDate(LocalDate birtDate) {
         this.birtDate = birtDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
